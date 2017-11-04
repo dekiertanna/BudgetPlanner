cb73ec05933b0140bfaff7f4170abe78bf6b9523
dekiertanna <dekiertanna@gmail.com>
2017-11-04 18:32:24 +0100

    Model portfel, wydatek, dochod

---
+++ Ciamajda/Ciamajda/Models/ExpenseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class ExpenseModel
+{
+    private int ID { get; set; }
+    private decimal expense { get; set; }
+    private string place { get; set; }
+    private DateTime date { get; set; }
+    private bool isCyclical { get; set; }
+    private int daysCycle { get; set; }
+    private string currency {get;set;}
+    private int categoryID { get; set; }
+    private int accountID { get; set; }
+
+	public ExpenseModel(int ID, decimal expense, string place, DateTime date, bool isCyclical, int daysCycle, string currency, int categoryID,int accountID)
+	{
+        this.ID = ID;
+        this.expense = expense;
+        this.place = place;
+        this.date = date;
+        this.isCyclical = isCyclical;
+        this.currency = currency;
+        this.categoryID = categoryID;
+        this.accountID = accountID;
+	}
+}
