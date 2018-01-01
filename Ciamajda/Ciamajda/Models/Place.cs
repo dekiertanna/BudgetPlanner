aa39925df5dce651dd2ff9bc91610958171a54ec
yaghaa <za.rogiem@gmail.com>
2018-01-01 19:06:45 +0100

    walidacja miejsca

--- Ciamajda/Ciamajda/Models/Place.cs
+++ Ciamajda/Ciamajda/Models/Place.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,21 @@ namespace Ciamajda
     public partial class Place
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [StringLength(100, ErrorMessage = "Pole {0} musi mieć conajmniej {2} i nie więcej niż {1} znaków.", MinimumLength = 1)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [StringLength(100, ErrorMessage = "Pole {0} musi mieć conajmniej {2} i nie więcej niż {1} znaków.", MinimumLength = 1)]
         public string Street { get; set; }
+
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [StringLength(100, ErrorMessage = "Pole {0} musi mieć conajmniej {2} i nie więcej niż {1} znaków.", MinimumLength = 1)]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Niepoprawny numer")]
         public int Number { get; set; }
 
         [Microsoft.AspNetCore.Mvc.HiddenInput(DisplayValue = false)]
