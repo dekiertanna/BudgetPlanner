b04ee1b5b6d5f5ca884b06610a73edb711f2c665
yaghaa <za.rogiem@gmail.com>
2018-01-01 19:26:14 +0100

    walidacja kategorii

--- Ciamajda/Ciamajda/Models/CategoryExpense.cs
+++ Ciamajda/Ciamajda/Models/CategoryExpense.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ciamajda
 {
     public partial class CategoryExpense
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [StringLength(100, ErrorMessage = "Pole {0} musi mieć conajmniej {2} i nie więcej niż {1} znaków.", MinimumLength = 1)]
         public string Name { get; set; }
+
         public string Description { get; set; }
 
         [Microsoft.AspNetCore.Mvc.HiddenInput(DisplayValue = false)]
